939d7e24b9829e0cea646730b99dcede0f96daab
Beefeater <myasoedov.blog@gmail.com>
2023-09-15 20:05:18 +0300

    add ts to useGetCat Hook

--- src/entities/cats/hooks/useGetCatsByBreed.tsx
+++ src/entities/cats/hooks/useGetCatsByBreed.tsx
@@ -2,12 +2,13 @@ import { useContext } from "react";
 import { useQuery } from "react-query";
 import BreedContext from "../context/BreedContext";
 import api from "../api/axios";
+import { Cat } from "../types/types";
 
 export default function useGetCatsByBreed(page = 1) {
   const context = useContext(BreedContext);
 
   if (!context) {
-    throw new Error("Компонент должен быть внутри BreedProvider");
+    throw new Error("Component should be used inside BreedProvider");
   }
 
   const { selectedBreed } = context;
@@ -15,7 +16,7 @@ export default function useGetCatsByBreed(page = 1) {
   const { data, error, isLoading } = useQuery(
     [selectedBreed, page],
     () =>
-      api.get("/images/search", {
+      api.get<Cat[]>("/images/search", {
         params: {
           breed_id: selectedBreed,
           limit: 10,
@@ -23,11 +24,9 @@ export default function useGetCatsByBreed(page = 1) {
         },
       }),
     {
-      enabled: !!selectedBreed, // запрос будет выполнен только если selectedBreed определен
+      enabled: !!selectedBreed,
     },
   );
 
   return { data: data?.data, error, isLoading };
 }
-
-// search?page=1&limit=10&breed_id=abob
