fc0d61060a0178d329228b4dd476a56ed1fd06f4
Beefeater <myasoedov.blog@gmail.com>
2023-09-15 20:05:50 +0300

    show Cat Card in Catalog

--- src/widgets/CatCatalog/components/CatCatalog.tsx
+++ src/widgets/CatCatalog/components/CatCatalog.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
 import NoCatsAvailable from "../../../pages/MainPage/components/NoCatsAvailable";
 import useGetCatsByBreed from "../../../entities/cats/hooks/useGetCatsByBreed";
+import CatCard from "./CatCard";
 
 export default function CatCatalog() {
   const { data, error, isLoading } = useGetCatsByBreed();
 
-  console.log(!data || data?.length === 0);
-
-  if (!data && data?.length === 0) {
+  if (!data || data?.length === 0) {
     return <NoCatsAvailable />;
   }
 
-  return null;
+  return (
+    <Row xs={1} md={2} lg={4}>
+      {data.map((cat) => (
+        <CatCard key={cat.id} cat={cat} />
+      ))}
+    </Row>
+  );
 }
