b9abf579baeecbb8194e5601c1e7c2b3c5259328
Beefeater <myasoedov.blog@gmail.com>
2023-09-18 12:03:34 +0300

    creacte Cat Page

--- src/pages/CatByIdPage/CatByIdPage.tsx
+++ src/pages/CatByIdPage/CatByIdPage.tsx
@@ -1,7 +1,22 @@
+import { useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import useGetCatById from "../../entities/cats/hooks/getCatById";
+
 export default function CatByIdPage() {
+  const { catId } = useParams();
+  const { data, error, isLoading } = useGetCatById({ url: catId });
+
+  if (!data) return null;
+
   return (
-    <div>
-      <h1>CatByIdPage</h1>
-    </div>
+    <Container>
+      <Row>
+        <Card>
+          <img src={data.url} alt="" />
+        </Card>
+      </Row>
+    </Container>
   );
 }
