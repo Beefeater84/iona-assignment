3d223dce1911946cf81426547120ed16d9d99169
Beefeater <myasoedov.blog@gmail.com>
2023-09-19 22:34:30 +0300

    small changes

--- src/entities/cats/api/axios.ts
+++ src/entities/cats/api/axios.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.thecatapi.com/v1/",
+  headers: {
+    "x-api-key": process.env.REACT_APP_CAT_API_KEY,
+  },
 });
 
 export default api;
