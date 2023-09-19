9722faf5643931d7b9d9730ad4c7988b1c9b7d09
Beefeater <myasoedov.blog@gmail.com>
2023-09-19 15:36:41 +0300

    add mocked Cats

---
+++ src/pages/MainPage/mocks/mockCats.ts
@@ -0,0 +1,49 @@
+import { Cat } from "../../../entities/cats/types/types";
+
+export const mockCats: Cat[] = [
+  {
+    id: "xnzzM6MBI",
+    url: "https://cdn2.thecatapi.com/images/xnzzM6MBI.jpg",
+    width: 2592,
+    height: 1629,
+  },
+  {
+    id: "EHG3sOpAM",
+    url: "https://cdn2.thecatapi.com/images/EHG3sOpAM.jpg",
+    width: 1600,
+    height: 1067,
+  },
+  {
+    id: "N-94oSJ5T",
+    url: "https://cdn2.thecatapi.com/images/N-94oSJ5T.jpg",
+    width: 1920,
+    height: 1280,
+  },
+  {
+    id: "9x1zk_Qdr",
+    url: "https://cdn2.thecatapi.com/images/9x1zk_Qdr.jpg",
+    width: 1204,
+    height: 1107,
+  },
+];
+
+export const mockCatsPage2: Cat[] = [
+  {
+    id: "g1j3wRjgx",
+    url: "https://cdn2.thecatapi.com/images/g1j3wRjgx.jpg",
+    width: 1024,
+    height: 1024,
+  },
+  {
+    id: "MJWtDz75E",
+    url: "https://cdn2.thecatapi.com/images/MJWtDz75E.jpg",
+    width: 1024,
+    height: 1024,
+  },
+  {
+    id: "p6x60nX6U",
+    url: "https://cdn2.thecatapi.com/images/p6x60nX6U.jpg",
+    width: 1032,
+    height: 774,
+  },
+];
