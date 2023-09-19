d1f5e289548a592a16137eebd444860381cfa01b
Beefeater <myasoedov.blog@gmail.com>
2023-09-19 16:46:30 +0300

    add catalog mock page 1

--- src/pages/MainPage/mocks/mockCats.ts
+++ src/pages/MainPage/mocks/mockCats.ts
@@ -27,6 +27,69 @@ export const mockCats: Cat[] = [
   },
 ];
 
+export const mockCatsPage1: Cat[] = [
+  {
+    id: "J2PmlIizw",
+    url: "https://cdn2.thecatapi.com/images/J2PmlIizw.jpg",
+    width: 1080,
+    height: 1350,
+  },
+  {
+    id: "LSaDk6OjY",
+    url: "https://cdn2.thecatapi.com/images/LSaDk6OjY.jpg",
+    width: 1080,
+    height: 1080,
+  },
+  {
+    id: "8ciqdpaO5",
+    url: "https://cdn2.thecatapi.com/images/8ciqdpaO5.jpg",
+    width: 1080,
+    height: 809,
+  },
+  {
+    id: "aaxNf4D0H",
+    url: "https://cdn2.thecatapi.com/images/aaxNf4D0H.jpg",
+    width: 1080,
+    height: 1350,
+  },
+  {
+    id: "UhqCZ7tC4",
+    url: "https://cdn2.thecatapi.com/images/UhqCZ7tC4.jpg",
+    width: 1600,
+    height: 1200,
+  },
+  {
+    id: "dN6eoeLjY",
+    url: "https://cdn2.thecatapi.com/images/dN6eoeLjY.jpg",
+    width: 3648,
+    height: 2736,
+  },
+  {
+    id: "H_UWbOfra",
+    url: "https://cdn2.thecatapi.com/images/H_UWbOfra.jpg",
+    width: 1200,
+    height: 1200,
+  },
+  {
+    id: "bz15V3Kvg",
+    url: "https://cdn2.thecatapi.com/images/bz15V3Kvg.jpg",
+    width: 1440,
+    height: 1080,
+  },
+  {
+    id: "NwMUoJYmT",
+    url: "https://cdn2.thecatapi.com/images/NwMUoJYmT.jpg",
+    width: 2160,
+    height: 1440,
+  },
+  {
+    id: "udZiLDG_E",
+    url: "https://cdn2.thecatapi.com/images/udZiLDG_E.jpg",
+    width: 880,
+    height: 1100,
+  },
+];
+
 export const mockCatsPage2: Cat[] = [
   {
     id: "g1j3wRjgx",
@@ -45,5 +108,47 @@ export const mockCatsPage2: Cat[] = [
     url: "https://cdn2.thecatapi.com/images/p6x60nX6U.jpg",
     width: 1032,
     height: 774,
+  },
+  {
+    id: "xnzzM6MBI",
+    url: "https://cdn2.thecatapi.com/images/xnzzM6MBI.jpg",
+    width: 2592,
+    height: 1629,
+  },
+  {
+    id: "EHG3sOpAM",
+    url: "https://cdn2.thecatapi.com/images/EHG3sOpAM.jpg",
+    width: 1600,
+    height: 1067,
+  },
+  {
+    id: "N-94oSJ5T",
+    url: "https://cdn2.thecatapi.com/images/N-94oSJ5T.jpg",
+    width: 1920,
+    height: 1280,
+  },
+  {
+    id: "9x1zk_Qdr",
+    url: "https://cdn2.thecatapi.com/images/9x1zk_Qdr.jpg",
+    width: 1204,
+    height: 1107,
+  },
+  {
+    id: "mjeF1hs8v",
+    url: "https://cdn2.thecatapi.com/images/mjeF1hs8v.jpg",
+    width: 800,
+    height: 528,
+  },
+  {
+    id: "POPfuPq8t",
+    url: "https://cdn2.thecatapi.com/images/POPfuPq8t.jpg",
+    width: 1024,
+    height: 768,
+  },
+  {
+    id: "xYLoht4o_",
+    url: "https://cdn2.thecatapi.com/images/xYLoht4o_.jpg",
+    width: 1080,
+    height: 1080,
   },
 ];
